0b677a09817bb3b6dc3f481255d26e137fe5abdf
ewagriusz <polskipiorun@gmail.com>
2021-10-12 23:54:17 +0200

    feat: basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-  import HelloWorld from './components/HelloWorld.vue'
+  import WeatherCard from './components/WeatherCard.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <button
-    id="dropdownMenuButton1"
-    class="btn btn-secondary dropdown-toggle"
-    type="button"
-    data-bs-toggle="dropdown"
-    aria-expanded="false"
-  >
-    bootstrap test
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
+  <main class="container border">
+    <WeatherCard />
+  </main>
 </template>
 
 <style>
@@ -28,8 +13,12 @@
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    padding: 3rem;
+  }
+
+  main {
+    display: grid;
+    place-items: center;
   }
 </style>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="row text-center"><h2>Weather ForeCast</h2></div>
+
+    <div class="row">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti itaque
+      sint dolorem totam voluptate voluptatibus! Corporis, laborum soluta!
+      Ratione perferendis tenetur nemo ducimus temporibus ad odit alias, numquam
+      veritatis quas!
+    </div>
+    <div class="row">
+      <label>
+        <input
+          class="form-control"
+          type="text"
+          name="peas"
+          placeholder="Location"
+        />
+      </label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #card-icon {
+    width: 30px;
+    height: 30px;
+  }
+
+  .container .row {
+    margin-bottom: 1rem;
+  }
+</style>
