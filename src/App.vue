d31812d04001754bf10b17b00a51e5d73bd0673d
ewagriusz <polskipiorun@gmail.com>
2021-10-12 18:23:34 +0200

    chore: add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <button
+    id="dropdownMenuButton1"
+    class="btn btn-secondary dropdown-toggle"
+    type="button"
+    data-bs-toggle="dropdown"
+    aria-expanded="false"
+  >
+    bootstrap test
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
 </template>
 
 <style>
