1e0031ac3c7e47c7621df4c6c2db9f2e375fd4b3
ewagriusz <polskipiorun@gmail.com>
2021-10-14 17:31:41 +0200

    fix: types

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -14,7 +14,6 @@
 <script setup lang="ts">
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-nocheck
-  /// <reference types="google.maps" />
   document.addEventListener('DOMContentLoaded', function () {
     // let autoCompleteScript = document.createElement('script')
     // autoCompleteScript.setAttribute(
