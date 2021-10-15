2ecd3b415c1ee5998ed74008318d07d1282e3031
ewagriusz <polskipiorun@gmail.com>
2021-10-15 17:11:46 +0200

    initial w3w input

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,35 +1,46 @@
 <template>
-  <label>
+  <div class="input-group mb-3">
     <input
-      id="autocomplete"
+      v-for="(varRef, name) in words"
+      :key="name"
+      v-model="varRef.value"
+      :placeholder="name"
       class="form-control"
-      type="text"
-      name="location"
-      placeholder="Location"
-      search-bar
     />
-  </label>
+  </div>
 </template>
 
 <script setup lang="ts">
+  import { ref, watch } from '@vue/runtime-core'
+  let word1 = ref('')
+  let word2 = ref('')
+  let word3 = ref('')
+  const words = {
+    'first word': word1,
+    'second word': word2,
+    'third word': word3,
+  }
+
+  watch([word1, word2, word3], () => {
+    if (word1.value !== '' && word2.value !== '' && word3.value !== '')
+      showPropositions()
+  })
+  function showPropositions() {
+    console.log(word1.value, word2.value, word3.value)
+  }
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-nocheck
-  document.addEventListener('DOMContentLoaded', function () {
-    // let autoCompleteScript = document.createElement('script')
-    // autoCompleteScript.setAttribute(
-    //   'src',
-    //   'https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&libraries=places&v=weekly&channel=1'
-    // )
-    // document.head.appendChild(autoCompleteScript)
 
-    function initialize() {
-      new google.maps.places.Autocomplete(
-        document.getElementById('autocomplete') as HTMLInputElement,
-        {
-          types: ['geocode'],
-        }
-      )
-    }
-    initialize()
-  })
+  // let autoCompleteScript = document.createElement('script')
+  // autoCompleteScript.setAttribute(
+  //   'src',
+  //   'https://assets.what3words.com/sdk/v3/what3words.js?key=L6RT3PJO'
+  // )
+  // document.head.appendChild(autoCompleteScript)
+
+  // what3words.api
+  //   .convertToCoordinates('dodawać.wręczyć.hodowla')
+  //   .then(function (response) {
+  //     console.log('[convertToCoordinates]', response)
+  //   })
 </script>
--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -4,29 +4,30 @@
 
 <template>
   <div class="container">
-    <div class="general-information">
+    <div class="general-information-section">
       <div class="status row text-center pt-3">
-        <h2>Enter a location</h2>
+        <h2>Enter a <a>W3W</a> location</h2>
       </div>
       <div class="location row text-center pb-3">
         <h5>To find the weather</h5>
       </div>
     </div>
+
     <div class="details-section row pt-6 pb-3">
       <div class="col text-center border-end">
         <h5>Wind</h5>
         <h6>val</h6>
       </div>
       <div class="col text-center border-end">
-        <h5>Wind</h5>
+        <h5>Temperature</h5>
         <h6>val</h6>
       </div>
       <div class="col text-center">
-        <h5>Wind</h5>
+        <h5>Precipitation</h5>
         <h6>val</h6>
       </div>
     </div>
-    <div class="row pb-3">
+    <div class="searchbar-section row pb-3">
       <SearchBar />
     </div>
   </div>
@@ -36,9 +37,5 @@
   #card-icon {
     width: 30px;
     height: 30px;
-  }
-
-  .container .row {
-    /* margin-bottom: 1rem; */
   }
 </style>
