85df9d59dce09dc6915fd8cdea14de5895e0062b
ewagriusz <polskipiorun@gmail.com>
2021-10-16 01:12:33 +0200

    feat: w3w to addres

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
+  import { ref } from '@vue/runtime-core'
+
   import SearchBar from './SearchBar.vue'
+  interface Address {
+    country: string
+    nearestPlace: string
+    words: string
+    rank: number
+    language: string
+  }
+  let data = ref({} as Address)
+  async function f(words: string) {
+    const response = await fetch(
+      `https://api.what3words.com/v3/convert-to-coordinates?words=${words}&key=L6RT3PJO`
+    )
+    const d = await response.json()
+    data.value = d
+  }
+  function getFlagEmoji(countryCode = 'fr') {
+    //really edgy way to get charEmoji from countryCode
+    return countryCode
+      .toUpperCase()
+      .replace(/./g, (char: string) =>
+        String.fromCodePoint(127397 + char.charCodeAt(0))
+      )
+  }
 </script>
 
 <template>
@@ -9,26 +34,36 @@
         <h2>Enter a <a>W3W</a> location</h2>
       </div>
       <div class="location row text-center pb-3">
-        <h5>To find the weather</h5>
+        <h5>
+          {{
+            data.country === undefined
+              ? 'To find the weather'
+              : getFlagEmoji(data?.country) +
+                ' ' +
+                data.nearestPlace +
+                ' - ' +
+                data.words
+          }}
+        </h5>
       </div>
     </div>
 
     <div class="details-section row pt-6 pb-3">
       <div class="col text-center border-end">
-        <h5>Wind</h5>
-        <h6>val</h6>
-      </div>
-      <div class="col text-center border-end">
         <h5>Temperature</h5>
         <h6>val</h6>
       </div>
+      <div class="col text-center border-end">
+        <h5>Pressure</h5>
+        <h6>val</h6>
+      </div>
       <div class="col text-center">
-        <h5>Precipitation</h5>
+        <h5>Humidity</h5>
         <h6>val</h6>
       </div>
     </div>
     <div class="searchbar-section row pb-3">
-      <SearchBar />
+      <SearchBar @newLocation="f" />
     </div>
   </div>
 </template>
