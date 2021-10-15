463cd67a555ba36bed6e27bd20998d2f5bd0e0eb
ewagriusz <polskipiorun@gmail.com>
2021-10-15 23:31:54 +0200

    feat: autocomplete w3w

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,6 +8,32 @@
       class="form-control"
     />
   </div>
+  <div class="flex">
+    <div
+      v-for="option in suggestions"
+      :key="option.words"
+      class="option"
+      @click="changeWords(option.words)"
+    >
+      <div class="input-group mb-2">
+        <span class="input-group-text words justify-start">
+          {{ [getFlagEmoji(option.country), option.nearestPlace].join(` `) }}
+        </span>
+        <!-- <span
+          v-for="word in option.words.split('.')"
+          :key="word"
+          class="input-group-text"
+        >
+          {{ word }}
+        </span> -->
+        <span class="input-group-text row">
+          <div v-for="word in option.words.split('.')" :key="word" class="col">
+            {{ word }}
+          </div>
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -20,13 +46,41 @@
     'second word': word2,
     'third word': word3,
   }
+  interface Option {
+    country: string
+    nearestPlace: string
+    words: string
+    rank: number
+    language: string
+  }
+  let suggestions = ref([] as Option[])
+
+  function changeWords(arg: string) {
+    ;[word1, word2, word3].map((el, ind) => (el.value = arg.split(`.`)[ind]))
+  }
 
   watch([word1, word2, word3], () => {
     if (word1.value !== '' && word2.value !== '' && word3.value !== '')
       processPropositions()
   })
-  function showPropositions() {
+  async function showPropositions() {
     console.log(word1.value, word2.value, word3.value)
+    const response = await fetch(
+      `https://api.what3words.com/v3/autosuggest?input=${[word1, word2, word3]
+        .map(q => q.value)
+        .join(`.`)}&key=L6RT3PJO`
+    )
+    const data = await response.json()
+    suggestions.value = data.suggestions as Option[]
+    console.log(suggestions.value)
+  }
+  function getFlagEmoji(countryCode: string) {
+    //really edgy way to get charEmoji from countryCode
+    return countryCode
+      .toUpperCase()
+      .replace(/./g, (char: string) =>
+        String.fromCodePoint(127397 + char.charCodeAt(0))
+      )
   }
   const debounce = <T extends (...args: unknown[]) => unknown>(
     callback: T,
@@ -59,3 +113,13 @@
   //     console.log('[convertToCoordinates]', response)
   //   })
 </script>
+
+<style scoped>
+  .words {
+    flex: auto;
+  }
+
+  .option {
+    cursor: pointer;
+  }
+</style>
