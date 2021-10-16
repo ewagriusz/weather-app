f5fedbf0cbb93b2d6243e66dd4da29cc431b575e
ewagriusz <polskipiorun@gmail.com>
2021-10-16 14:29:51 +0200

    feat: working weather-api

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -2,6 +2,13 @@
   import { ref } from '@vue/runtime-core'
 
   import SearchBar from './SearchBar.vue'
+  interface weather {
+    temperature: string
+    pressure: string
+    humidity: string
+  }
+  let currentWeather = ref({} as weather)
+
   interface Address {
     country: string
     nearestPlace: string
@@ -10,12 +17,19 @@
     language: string
   }
   let data = ref({} as Address)
-  async function f(words: string) {
+  async function proccesNewLocation(words: string) {
     const response = await fetch(
       `https://api.what3words.com/v3/convert-to-coordinates?words=${words}&key=L6RT3PJO`
     )
     const d = await response.json()
     data.value = d
+    const weatherResponse = await fetch(
+      `https://fcc-weather-api.glitch.me/api/current?lat=${d.coordinates.lat}&lon=${d.coordinates.lng}`
+    )
+    const weather = await weatherResponse.json()
+    currentWeather.value.temperature = weather.main.temp
+    currentWeather.value.pressure = weather.main.pressure
+    currentWeather.value.humidity = weather.main.humidity
   }
   function getFlagEmoji(countryCode = 'fr') {
     //really edgy way to get charEmoji from countryCode
@@ -31,7 +45,16 @@
   <div class="container">
     <div class="general-information-section">
       <div class="status row text-center pt-3">
-        <h2>Enter a <a>W3W</a> location</h2>
+        <h2>
+          Enter a
+          <a
+            href="https://what3words.com/about/"
+            class="link-primary"
+            title="go to what3words.com to check what is it"
+            >W3W</a
+          >
+          location
+        </h2>
       </div>
       <div class="location row text-center pb-3">
         <h5>
@@ -51,19 +74,25 @@
     <div class="details-section row pt-6 pb-3">
       <div class="col text-center border-end">
         <h5>Temperature</h5>
-        <h6>val</h6>
+        <h6>
+          {{
+            currentWeather?.temperature === undefined
+              ? 'n/a'
+              : `${Number(currentWeather?.temperature).toFixed(0)}°`
+          }}
+        </h6>
       </div>
       <div class="col text-center border-end">
         <h5>Pressure</h5>
-        <h6>val</h6>
+        <h6>{{ currentWeather?.pressure ?? 'n/a' }}</h6>
       </div>
       <div class="col text-center">
         <h5>Humidity</h5>
-        <h6>val</h6>
+        <h6>{{ currentWeather?.humidity ?? 'n/a' }}</h6>
       </div>
     </div>
     <div class="searchbar-section row pb-3">
-      <SearchBar @newLocation="f" />
+      <SearchBar @newLocation="proccesNewLocation" />
     </div>
   </div>
 </template>
